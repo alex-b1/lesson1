1973d1afa6650309afd6656d3a2e417631e1af4d
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-04-07 10:34:40 +0000

    fix div

--- 2.rb
+++ 2.rb
@@ -6,6 +6,6 @@ base = gets.chomp.to_i
 print 'Введите высоту треугольника: '
 height = gets.chomp.to_i
 
-area = ((1.fdiv 2) * base * height).round(2)
+area = ((1 / 2.0) * base * height).round(2)
 
 print "Площадь треугольника: #{area}"
